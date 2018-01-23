87d7cb96c1a4281b9458427adc840d4191accbc9
Mina Slater <slater.mina@gmail.com>
2018-01-23 11:36:04 -0600

    adds initial js html css files

---
+++ script.js
@@ -0,0 +1,65 @@
+var $ = function(selector) {
+  var element = document.querySelectorAll(selector);
+  var text = function(newText) {
+    for (var i = 0; i < element.length; i++) {
+      element[i].innerText = newText;
+    }
+  };
+
+  var addClass = function(className) {
+    for (var i = 0; i < element.length; i++) {
+      element[i].classList.add(className);
+    }
+  };
+
+  var removeClass = function(className) {
+    for (var i = 0; i < element.length; i++) {
+      element[i].classList.remove(className);
+    }
+  };
+
+  var on = function(action, cb) {
+    for (var i = 0; i < element.length; i++) {
+      element[i].addEventListener(action, cb);
+    }
+  };
+
+  //toggleClass
+  var toggleClass = function(newClass) {
+    for (var i = 0; i < element.length; i++) {
+      var elemClassList = element[i].classList;
+      console.log(elemClassList);
+      var isAlreadyAClass = false;
+      for (var j = 0; j < elemClassList.length; j++) {
+        if (elemClassList[j] === newClass) {
+          isAlreadyClass = true;
+        }
+      }
+      if (isAlreadyAClass) {
+        addClass(newClass);
+      } else {
+        removeClass(newClass);
+      }
+    }
+  };
+
+  //html
+  var html = function(newHTML) {
+    for (var i = 0; i < element.length; i++) {
+      element[i].innerHTML = newHTML;
+    }
+  };
+
+  var publicAPI = {
+    element: element,
+    text: text,
+    addClass: addClass,
+    removeClass: removeClass,
+    on: on,
+    html: html,
+    toggleClass: toggleClass
+  };
+
+  return publicAPI;
+};
+
